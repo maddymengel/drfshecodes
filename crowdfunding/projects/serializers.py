bb9c77fdd6be50d80ea47ee49e60fdb770940524
Maddy Mengel <maddy.mengel@gmail.com>
2023-08-10 17:18:45 +1000

    Users

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -3,11 +3,13 @@ from django.apps import apps
 
 
 class PledgeSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.id') #?
     class Meta:
         model = apps.get_model('projects.Pledge')
         fields = '__all__'
 
 class ProjectSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.id')
     class Meta:
         model = apps.get_model('projects.Project')
         fields = '__all__'
